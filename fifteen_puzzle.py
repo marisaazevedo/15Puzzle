63f7041d51a62604cdc17ff8240f6867b08fd79b
Marisa Azevedo <marisa.azevedo@Marisas-MacBook-Pro.local>
2023-03-01 14:30:40 +0000

    chamar funcao pretendida

--- fifteen_puzzle.py
+++ fifteen_puzzle.py
@@ -7,23 +7,35 @@ def main():
 
     initial = list(map(int, input().split()))
     final = list(map(int, input().split()))
+    method = int(input())
 
     if solvability(initial) == solvability(final):
-        start = time.time()
-        print("Depth-First-Search: %d passos" %dfs(initial, final))
-        end = time.time()
-        print("time = %f segundos" %(end - start))
-        #print("Breadth-First-Search: ", bfs(initial, final))
-        #print("Iterative-Deepening-Search: ", ids(initial, final))
-        #print("Greedy: ", g(initial,final))
-        #print("A*: ", a(initial,final))
+        method = int(input())
+        if(method) == "dfs":
+            start = time.time()
+            print("Depth-First-Search: %d passos" %dfs(initial, final))
+            end = time.time()
+            print("time = %f segundos" %(end - start))
+            print("memory = ...")
+        elif(method) == "bfs":
+            # print("Breadth-First-Search: ", bfs(initial, final))
+            print("não está implementado")
+        elif(method) == "ids":
+            # print("Iterative-Deepening-Search: ", ids(initial, final))
+            print("não está implementado")
+        elif(method) == "greedy":
+            # print("Greedy: ", g(initial,final))
+            print("não está implementado")
+        elif(method) == "A*":
+            # print("A*: ", a(initial,final))
+            print("não está implementado")
     else:
         print("There is no path between the final configuration and the initial configuration.")
 
     # configurações dadas em formato de tabuleiro
-    #print("Initial Configuration:")
-    #print(puzzle(initial))
-    #print("Final Configuration:")
-    #print(puzzle(final))
+    # print("Initial Configuration:")
+    # print(puzzle(initial))
+    # print("Final Configuration:")
+    # print(puzzle(final))
 
 main()
