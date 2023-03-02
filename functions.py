011eb14627119f81cfd88259ae4d06311a8a3e95
Marisa Azevedo <marisa.azevedo@Marisas-MacBook-Pro.local>
2023-03-02 10:21:16 +0000

    comentarios

--- functions.py
+++ functions.py
@@ -68,17 +68,17 @@ def greedy_misplaced(i,f):
 def greedy_manhattan(i,f):
     return 0
 
-## 1: GeneralSearchAlgorithm(QueueingFunction,configInicial,configFinal)
-## 2: if thereIsNoSolution(configInicial,configFinal) then
-## 3:    return “It is impossible to reach a solution”
-## 4: end if
-## 5: queue = configInicial
-## 6: while queue not empty do
-## 7:    node = removeFrontNodeFrom(queue)
-## 8:    if node is solution then
-## 9:        return Path to solution
-## 10:   end if
-## 11:   descendantList = MakeDescendants(node)
-## 12:   insert(descendantList,queue,QueueingFunction)
-## 13: end while
-## 14: return “solution not found”
+# 1: GeneralSearchAlgorithm(QueueingFunction,configInicial,configFinal)
+# 2: if thereIsNoSolution(configInicial,configFinal) then
+# 3:    return “It is impossible to reach a solution”
+# 4: end if
+# 5: queue = configInicial
+# 6: while queue not empty do
+# 7:    node = removeFrontNodeFrom(queue)
+# 8:    if node is solution then
+# 9:        return Path to solution
+# 10:   end if
+# 11:   descendantList = MakeDescendants(node)
+# 12:   insert(descendantList,queue,QueueingFunction)
+# 13: end while
+# 14: return “solution not found”
