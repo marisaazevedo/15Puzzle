864e587b7fce293b1dcaa56656bb539727c49158
Marisa Azevedo <marisa.azevedo@Marisas-MacBook-Pro.local>
2023-03-02 10:16:38 +0000

    comentarios

--- functions.py
+++ functions.py
@@ -1,21 +1,21 @@
 from auxFunctions import Puzzle
 from collections import deque
 
-def dfs(root: list[int], final: list[int]) -> Puzzle: #retornar a configuracao ou seja o puzzle??
-    stack = deque() #criar uma pilha
-    stack.append(Puzzle(root)) # adicionar a matriz atual a pilha
-    visited = set() # criar um tuplo para as matrizes visitadas
+def dfs(root: list[int], final: list[int]) -> Puzzle:
+    stack = deque()                         # criar uma pilha
+    stack.append(Puzzle(root))              # adicionar a configuracao atual à pilha
+    visited = set()                         # criar uma lista com apenas as configuracoes visitadas
 
     while len(stack):
-        puzzle = stack.pop() # atribuir à variável puzzle o último elemento da pilha
+        puzzle = stack.pop()                # atribuir à variável puzzle o último elemento da pilha e retira-o da pilha
         print(puzzle.array, len(visited))
 
-        if tuple(puzzle.array) in visited: #verificar se puzzle já visitamos
+        if tuple(puzzle.array) in visited:  # verificar se configuracao atual já foi visitada
             continue
         visited.add(tuple(puzzle.array))
 
-        if puzzle.array == final: #comparar a configuração atual com a pretendida
-            return puzzle.depth
+        if puzzle.array == final:           # verificar se a configuração atual é igual à configuracao final
+            return puzzle.depth             # retorna o números passos para chegar da configuracao inicial à final
 
         if tuple(puzzle.left())not in visited:
             stack.append(Puzzle(puzzle.left(), depth = puzzle.depth + 1))
@@ -29,20 +29,20 @@ def dfs(root: list[int], final: list[int]) -> Puzzle: #retornar a configuracao o
     raise Exception("Puzzle cannot be solved")
 
 def bfs(root: list[int], final: list[int]):
-    queue = deque()
-    queue.append(Puzzle(root))
-    visited = set()
+    queue = deque()                             # criar uma fila
+    queue.append(Puzzle(root))                  # adicionar a configuracao atual à fila
+    visited = set()                             # criar uma lista com apenas as configuracoes visitadas
 
     while len(queue):
-        puzzle = queue.popleft() # atribuir à variável puzzle o último elemento da pilha
+        puzzle = queue.popleft()                # atribuir à variável puzzle o último elemento da pilha
         print(puzzle.array, len(visited))
 
-        if tuple(puzzle.array) in visited: #verificar se puzzle já visitamos
+        if tuple(puzzle.array) in visited:      # verificar se configuracao atual já foi visitada
             continue
         visited.add(tuple(puzzle.array))
 
-        if puzzle.array == final: #comparar a configuração atual com a pretendida
-            return puzzle.depth
+        if puzzle.array == final:               # verificar se a configuração atual é igual à configuracao final
+            return puzzle.depth                 # retorna o números passos para chegar da configuracao inicial à final
 
         if tuple(puzzle.left())not in visited:
             queue.append(Puzzle(puzzle.left(), depth = puzzle.depth + 1))
@@ -68,17 +68,17 @@ def greedy_misplaced(i,f):
 def greedy_manhattan(i,f):
     return 0
 
-##1: GeneralSearchAlgorithm(QueueingFunction,configInicial,configFinal)
-##2: if thereIsNoSolution(configInicial,configFinal) then
-##3:    return “It is impossible to reach a solution”
-##4: end if
-##5: queue = configInicial
-##6: while queue not empty do
-##7:    node = removeFrontNodeFrom(queue)
-##8:    if node is solution then
-##9:        return Path to solution
-##10:   end if
-##11:   descendantList = MakeDescendants(node)
-##12:   insert(descendantList,queue,QueueingFunction)
-##13: end while
-##14: return “solution not found”
+## 1: GeneralSearchAlgorithm(QueueingFunction,configInicial,configFinal)
+## 2: if thereIsNoSolution(configInicial,configFinal) then
+## 3:    return “It is impossible to reach a solution”
+## 4: end if
+## 5: queue = configInicial
+## 6: while queue not empty do
+## 7:    node = removeFrontNodeFrom(queue)
+## 8:    if node is solution then
+## 9:        return Path to solution
+## 10:   end if
+## 11:   descendantList = MakeDescendants(node)
+## 12:   insert(descendantList,queue,QueueingFunction)
+## 13: end while
+## 14: return “solution not found”
